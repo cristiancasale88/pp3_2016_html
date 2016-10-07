6f48edd97942c8c2d949e5bed39181d8b5283ddb
Santycabj <g.manez@itecriocuarto.org.ar>
2016-10-07 18:02:08 -0300

    subida index

---
+++ web/js/sgmanez/miprogramacion.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#Series").click(function(){
+        $("#myCarousel").load("series.html");
+    });
+    $("#Contactos").click(function(){
+        $("#myCarousel").load("contactos.html");
+    });
+    $("#Perfil").click(function(){
+        $("#myCarousel").load("perfil.html");
+    });
+    $("#Ver").click(function(){
+        $("#myCarousel").load("series.html");
+    });
+    $("#Vip").click(function(){
+        $("#myCarousel").load("contactos.html");
+    });
+});
---
+++ web/js/sgmanez/miprogramacion2.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#sitio").click(function(){
+        $("#myCarousel").load("walking.html");
+    });
+});
